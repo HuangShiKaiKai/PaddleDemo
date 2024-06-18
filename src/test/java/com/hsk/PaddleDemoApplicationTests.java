486ce731cbe7b9efbe84bcb5b1983af039a35dc5
黄世凯 <huangshikaikai@163.com>
2024-06-18 11:13:16 +0800

    a

--- src/test/java/com/hsk/PaddleDemoApplicationTests.java
+++ src/test/java/com/hsk/PaddleDemoApplicationTests.java
@@ -5,6 +5,8 @@ import com.hsk.utils.PaddleServingUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
+
 
 @SpringBootTest
 class PaddleDemoApplicationTests {
@@ -68,5 +70,35 @@ class PaddleDemoApplicationTests {
         String labelParentPath = "F:\\product0\\Label.txt";
         String targetPathRoot = "F:\\productAll";
         DataSetUtil.copyLabelImage(labelParentPath, targetPathRoot);
+    }
+    @Test
+    void test4() {
+        String dir = "F:\\飞浆\\out\\img";
+        String dir1 = "F:\\productAll";
+        String dir2 = "F:\\labelOcr";
+        //读取dir所有文件名。如果dir1和dir2存在同名文件，则删除dir1和dir2的同名文件
+        File dirFile = new File(dir);
+        if (dirFile.isDirectory()) {
+            File[] files = dirFile.listFiles();
+            for (File file : files) {
+                if (file.isFile()) {
+                    String fileName = file.getName();
+                    File file1 = new File(dir1, fileName);
+                    File file2 = new File(dir2, fileName);
+
+                    // 检查dir1中是否存在同名文件，如果存在则删除
+                    if (file1.exists()) {
+                        file1.delete();
+                        System.out.println("Deleted: " + file1.getAbsolutePath());
+                    }
+
+                    // 检查dir2中是否存在同名文件，如果存在则删除
+                    if (file2.exists()) {
+                        file2.delete();
+                        System.out.println("Deleted: " + file2.getAbsolutePath());
+                    }
+                }
+            }
+        }
     }
 }
